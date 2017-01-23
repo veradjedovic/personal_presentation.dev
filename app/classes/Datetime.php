33d01f84fb73486df829bd2abaa1b82f9709acd5
frog <verajovanovicbg@hotmail.com>
2017-01-23 18:30:51 +0100

    Added new methods in Datetime class

--- app/classes/Datetime.php
+++ app/classes/Datetime.php
@@ -5,8 +5,49 @@ namespace app\classes;
 /**
  * Description of Datetime
  *
- * @author frog
+ * @author Vera
  */
-class Datetime {
-    //put your code here
+class Datetime 
+{
+    /**
+     *
+     * @var string
+     */
+    protected $yearBegin;
+    
+    /**
+     *
+     * @var string
+     */
+    protected $yearEnd;
+    
+    
+    /**
+     * 
+     * @param string/int $yearBegin
+     * @param string/int $yearEnd
+     */
+    public function __construct($yearBegin, $yearEnd) 
+    {
+        $this->yearBegin = $yearBegin;
+        $this->yearEnd = $yearEnd;
+    }
+    
+    /**
+     * 
+     * @return string/int
+     */
+    public function getYearBegin() 
+    {
+        return $this->yearBegin;
+    }
+
+    /**
+     * 
+     * @return string/int
+     */
+    public function getYearEnd() 
+    {
+        return $this->yearEnd;
+    }
 }
