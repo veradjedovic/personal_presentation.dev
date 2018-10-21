464ce2bba20a0cef77593ef6105356fa62993c7c
veradjedovic <verajovanovicbg@hotmail.com>
2018-10-21 18:05:08 +0200

    Fix login page

--- views/login.php
+++ views/login.php
@@ -6,7 +6,7 @@
 <!-- BEGIN HEAD -->
 <head>
      <meta charset="UTF-8" />
-    <title>BCORE Admin Dashboard Template | Login Page</title>
+    <title>Portfolio | Login Page</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 	<meta content="" name="description" />
 	<meta content="" name="author" />
